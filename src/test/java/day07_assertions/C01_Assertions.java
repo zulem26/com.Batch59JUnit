1000835ec11e516f57cb98de35bf0cd2d9fba64e
zulem26 <pruvanetastore@gmail.com>
2022-05-16 23:23:31 +0300

    7.gun

---
+++ src/test/java/day07_assertions/C01_Assertions.java
@@ -0,0 +1,61 @@
+package day07_assertions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C01_Assertions {
+    /*
+        amazon anasayfaya gidin
+        3 farkli test methodu olusturarak asagidaki gorevleri yapin
+        1- Url in amazon icerdigini test edin
+        2- title in facebook icermedigini test edin
+        3- sol ust kosede amazon logosunun gorundugunu test edin
+     */
+    static WebDriver driver;
+    @BeforeClass
+    public static void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+    @AfterClass
+    public static void tearDown(){
+        driver.close();
+    }
+
+    @Test
+    public void test01(){
+        driver.get("https://www.amazon.com");
+
+        //1- Url in amazon icerdigini test edin
+        String arananKelime = "amazon";
+        String actualUrl = driver.getCurrentUrl();
+        Assert.assertTrue(actualUrl.contains(arananKelime));
+    }
+
+    @Test
+    public void test02(){
+        // 2- title in facebook icermedigini test edin
+        String istenmeyenKelime = "facebook";
+        String actualTitle = driver.getTitle();
+        Assert.assertFalse(actualTitle.contains(istenmeyenKelime));
+
+    }
+
+    @Test
+    public void test03(){
+        //3- sol ust kosede amazon logosunun gorundugunu test edin
+        WebElement logoElementi = driver.findElement(By.id("nav-logo-sprites"));
+        Assert.assertTrue(logoElementi.isDisplayed());
+
+    }
+
+
+}
---
+++ src/test/java/day07_assertions/C04_DropDownMenu.java
@@ -0,0 +1,64 @@
+package day07_assertions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.time.Duration;
+
+public class C04_DropDownMenu {
+    /* amazon a gidip
+       dropdown dan books secenegini secip java aratalim
+       arama sonuclarinin Java icerdigini test edelim
+     */
+    WebDriver driver;
+
+    @Before
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+    }
+
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
+
+    @Test
+    public void test01() throws InterruptedException {
+        driver.get("https://www.amazon.com");
+        // dropdown dan bir option secmek icin 3 adim vardir
+        //1- adim dropdown i locate edelim
+        WebElement dropDownMenu = driver.findElement(By.xpath("//select[@id='searchDropdownBox']"));
+
+        // 2- bir select objesi olusturup
+        // parametre olarak bir onceki adimda locate ettigimiz ddm u girelim
+        Select select = new Select(dropDownMenu);
+
+        // 3- Dropdown da var olan option lardan istedigimiz bir taneyi secelim
+        select.selectByVisibleText("Books");
+        // select.selectByIndex();
+        // select.selectByValue();
+
+        // arama kutusuna Java yazdiralim
+        WebElement aramaKutusu = driver.findElement(By.id("twotabsearchtextbox"));
+        aramaKutusu.sendKeys("java" + Keys.ENTER);
+
+        WebElement sonucYazisiElementi = driver.findElement(By.xpath("(//div[@class='a-section a-spacing-small a-spacing-top-small'])[1]"));
+        String sonucYazisiStr = sonucYazisiElementi.getText();
+        String arananKelime = "Java";
+        Assert.assertTrue(sonucYazisiStr.contains(arananKelime));
+        Thread.sleep(5000);
+    }
+}
